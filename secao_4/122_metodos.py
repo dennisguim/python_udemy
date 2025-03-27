b581484b1a46f42ac2e3622e7df80b95f07f949e
dennisguim <drinaldim@gmail.com>
2025-03-27 10:30:22 -0300

    Create 122_metodos.py

---
+++ secao_4/122_metodos.py
@@ -0,0 +1,23 @@
+# SHALLOW COPY E DEEP COPY
+
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+
+pessoa = {
+    'nome': 'Dennis',
+    'sobrenome': 'Guimaraes',
+}
+
+print(list(pessoa.items()))
+
+for chave, valor in pessoa.items():
+    print(chave, valor)
