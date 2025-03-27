fb0cd44c78dda0a4d10d8ae92f202cd1fd8a1808
dennisguim <drinaldim@gmail.com>
2025-03-27 10:50:10 -0300

    Create 124_exercicio.py

---
+++ secao_4/124_exercicio.py
@@ -0,0 +1,17 @@
+perguntas = [
+    {
+        'Pergunta': 'Quanto é 2 + 2?',
+        'Opções': ['1', '3', '4', '5'],
+        'Resposta': '4',  
+    },
+    {
+        'Pergunta': 'Quanto é 5 x 5?',
+        'Opções': ['15', '25', '30', '35'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10 % 2:',
+        'Opções': ['4', '7', '3', '5'],
+        'Reposta': '5',
+    }
+]
