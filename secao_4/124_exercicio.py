9802cda1403742f27921239281072c2f5f356397
dennisguim <drinaldim@gmail.com>
2025-03-27 16:21:44 -0300

    Update 124_exercicio.py

--- secao_4/124_exercicio.py
+++ secao_4/124_exercicio.py
@@ -16,12 +16,50 @@ perguntas = [
     }
 ]
 
-while True:
-    lista_chaves = list(perguntas[0].keys())
-    print(perguntas[0]['Pergunta'],'\n', '\n',
-          lista_chaves[1]
+qtd_chaves = len(perguntas)
+n = 0
+contador = qtd_chaves - n
+i = 0
+
+while contador != 1:
+    lista_chaves = list(perguntas[i].keys())
+    qtd_opcoes = len(perguntas[i]['Opções'])
+    lista1 = list(perguntas[i]['Opções'])
+    
+    print("\n",
+        perguntas[i]['Pergunta'],'\n','\n',
+        lista_chaves[1], '\n',
+        '0)', lista1[0], '\n',
+        '1)', lista1[1], '\n',
+        '2)', lista1[2], '\n',
+        '3)', lista1[3], '\n',
           )
 
+    resp = int(input('Escolha uma opção: '))
+    resp_lista = lista1[resp]
+    
+    
+    print(resp)
+    print(perguntas[i]['Resposta'])
+
+    print("\n" "Resposta errada!" if resp_lista != perguntas[i]['Resposta'] else "\n" "Correto")
+    i += 1
+    n += 1
 
 
-    resp = input('Escolha uma opção: ')
+    """ lista_chaves = list(perguntas[1].keys())
+    qtd_opcoes = len(perguntas[1]['Opções'])
+    lista1 = list(perguntas[1]['Opções'])
+
+    print("\n",
+        perguntas[1]['Pergunta'],'\n','\n',
+        lista_chaves[1], '\n',
+        '0)', lista1[0], '\n',
+        '1)', lista1[1], '\n',
+        '2)', lista1[2], '\n',
+        '3)', lista1[3], '\n',
+          )
+
+    resp = input('Escolha uma opção: ') 
+
+    print("\n" "Resposta errada!" if resp != '1'  else "Correto") """
