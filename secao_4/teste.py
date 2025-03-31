952bc8b8f9fe25639987ab39cc0f5e18780c0158
dennisguim <drinaldim@gmail.com>
2025-03-31 14:08:40 -0300

    Update teste.py

--- secao_4/teste.py
+++ secao_4/teste.py
@@ -1,21 +1,8 @@
-perguntas = [
-    {
-        'Pergunta': 'Quanto é 2+2?',
-        'Opções': ['1', '3', '4', '5'],
-        'Resposta': '4',
-    },
-    {
-        'Pergunta': 'Quanto é 5*5?',
-        'Opções': ['25', '55', '10', '51'],
-        'Resposta': '25',
-    },
-    {
-        'Pergunta': 'Quanto é 10/2?',
-        'Opções': ['4', '5', '2', '1'],
-        'Resposta': '5',
-    },
-]
+s1 = set()
+s1.add('dennis')
+s1.add(1)
+s1.update(('guim', 1, 2, 3, 4))
+s1.discard('dennis')
+s1.discard(1)
 
-for i, j in enumerate(perguntas[0]['Opções']):
-    print(i, j)
-# enumerate é um iterável, ele numera a lista e retorna uma tupla no caso de usar o for.
+print(s1)
