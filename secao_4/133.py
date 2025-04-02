6eef21c5aafcebabf3e8b9294a7514aaa3890cdf
dennisguim <drinaldim@gmail.com>
2025-04-02 08:42:38 -0300

    Create 133.py

---
+++ secao_4/133.py
@@ -0,0 +1,14 @@
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+def ordena(item):
+    print(item)
+    return item
+
+
+print(ordena(lista))
